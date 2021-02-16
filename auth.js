d14fdb09a15fcfe36234a4db3b38e2ec90e37489
Nate <nate@natereed.com>
2021-02-15 18:56:03 -0600

    Add secured route example - user profile

--- auth.js
+++ auth.js
@@ -1,5 +1,10 @@
 var passport = require("passport");
 var LocalStrategy = require("passport-local").Strategy;
+const JWTstrategy = require('passport-jwt').Strategy;
+const ExtractJWT = require('passport-jwt').ExtractJwt;
+
+// TODO: remove this duplication (it's also declared in routes.js).
+const jwt_secret_key = 'eN}M<9(+^WkL:R,.&C,kw.jvn%Zq50dhlF2+?f}iJ<n!zPc?0^#,BwRd29TXddu';
 
 var User = require("./user");
 
@@ -15,33 +20,51 @@ module.exports = function() {
         });
     });
 
+    passport.use(
+        new JWTstrategy(
+            {
+                secretOrKey: jwt_secret_key,
+                jwtFromRequest: ExtractJWT.fromUrlQueryParameter('secret_token')
+            },
+            async (token, done) => {
+                try {
+                    return done(null, token.user);
+                } catch (error) {
+                    done(error);
+                }
+            }
+        )
+    );
+
     passport.use("login", new LocalStrategy({
             usernameField: 'email',
             passwordField: 'password'
         },
         async (username, password, done) => {
-        console.log("login...");
+            console.log("login...");
+            console.log(username);
+            console.log(password);
 
-        User.findOne({ email: username }, function(err, user) {
-            if (err) { return done(err); }
-            if (!user) {
-                return done(null, false, { message: "User not found!" });
-            }
-            user.checkPassword(password, function(err, isMatch) {
-                console.log("Checked password...");
-                console.log("Error? Match?");
-                console.log(err);
-                console.log(isMatch);
+            User.findOne({ email: username }, function(err, user) {
                 if (err) { return done(err); }
-                if (isMatch) {
-                    console.log("Returning done...");
-                    return done(null, user, { message: 'Logged in Successfully' });
-                } else {
-                    return done(null, false, { message: "Invalid password." });
+                if (!user) {
+                    return done(null, false, { message: "User not found!" });
                 }
+                user.checkPassword(password, function(err, isMatch) {
+                    console.log("Checked password...");
+                    console.log("Error? Match?");
+                    console.log(err);
+                    console.log(isMatch);
+                    if (err) { return done(err); }
+                    if (isMatch) {
+                        console.log("Returning done...");
+                        return done(null, user, { message: 'Logged in Successfully' });
+                    } else {
+                        return done(null, false, { message: "Invalid password." });
+                    }
+                });
             });
-        });
-    }));
+        }));
 
     // passport.use('signup', new LocalStrategy(
     //     {
--- routes.js
+++ routes.js
@@ -1,6 +1,10 @@
+
+
 var express = require("express");
 var passport = require("passport");
 const jwt = require('jsonwebtoken');
+
+const jwt_secret_key = 'eN}M<9(+^WkL:R,.&C,kw.jvn%Zq50dhlF2+?f}iJ<n!zPc?0^#,BwRd29TXddu';
 
 var User = require("./user");
 var router = express.Router();
@@ -18,10 +22,55 @@ router.use(function(req, res, next) {
     next();
 });
 
-router.post("/login", passport.authenticate("login"), function(req, res) {
-    // TODO - generate JWT
-    res.status(200).send({"msg": "OK"});
-});
+// router.post("/login", passport.authenticate("login"), function(req, res) {
+//     // TODO - generate JWT
+//     res.status(200).send({"msg": "OK"});
+// });
+
+router.post(
+    '/login',
+    async (req, res, next) => {
+        console.log("Logging in...");
+        passport.authenticate(
+            'login',
+            async (err, user, info) => {
+
+                console.log("Error: " + err);
+                console.log("User:" + user);
+                console.log("Info:" + info);
+                try {
+                    console.log("User: " + user);
+
+                    if (err) {
+                        console.log("An error occurred... returning next(error)")
+                        const error = new Error('An error occurred.');
+
+                        return next(error);
+                    }
+
+                    if (!user) {
+                        return res.status(401).json({ "message": "No user found for username, password."})
+                    }
+
+                    req.login(
+                        user,
+                        { session: false }, // Should not store in session
+                        async (error) => {
+                            if (error) return next(error);
+
+                            const body = { _id: user._id, email: user.email };
+                            const token = jwt.sign({ user: body }, jwt_secret_key);
+
+                            return res.json({ token });
+                        }
+                    );
+                } catch (error) {
+                    return next(error);
+                }
+            }
+        )(req, res, next);
+    }
+);
 
 router.post("/signup", async function(req, res, next) {
 
@@ -52,6 +101,17 @@ router.post("/signup", async function(req, res, next) {
             message: "Signup successful",
             user: req.user
         });
+    }
+);
+
+router.get(
+    '/profile',
+    (req, res, next) => {
+        res.json({
+            message: 'You made it to the secure route',
+            user: req.user,
+            token: req.query.secret_token
+        })
     }
 );
 
